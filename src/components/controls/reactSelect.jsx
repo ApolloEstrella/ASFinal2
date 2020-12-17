a0e1cd53932298c6cb7903f187de8a84aed292a2
ApolloEstrella <34788252+ApolloEstrella@users.noreply.github.com>
2020-12-17 20:32:59 +0800

    Updates

--- src/components/controls/reactSelect.jsx
+++ src/components/controls/reactSelect.jsx
@@ -15,6 +15,7 @@ import {
   useMediaQuery,
   useTheme,
 } from "@material-ui/core";
+import * as Yup from "yup";
 
 const ReactSelect = ({
   options,
@@ -53,7 +54,7 @@ const ReactSelect = ({
   };
   const [dialogValue, setDialogValue] = useState({
     name: "",
-    billingAddress: ""
+    billingAddress: "",
   });
 
   const [billingAddress, setBillingAddress] = useState({
@@ -87,13 +88,13 @@ const ReactSelect = ({
     setFieldTouched(props.name, true);
   }
 
-  function handleNewCustomer() {
+  function handleNewCustomer(name, address) {
     fetch("https://localhost:44302/api/subsidiaryledger/addaccount", {
       method: "POST",
       body: JSON.stringify({
         id: 0,
-        name: dialogValue.name,
-        address: billingAddress.address,
+        name: name,
+        address: address,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -105,11 +106,17 @@ const ReactSelect = ({
         handleClose();
       });
   }
-   
+
+  const validationSchemaCustomer = Yup.object().shape({
+    name: Yup.string().required("Enter Customer Name."),
+    //billingAddress: Yup.string().required("Enter Billing Address."),
+  });  
+
+
   const initialCustomerValues = {
     name: dialogValue.name,
-    billingAddress: ""
-  }
+    billingAddress: "",
+  };
   return (
     <React.Fragment>
       <CreatableSelect
@@ -134,66 +141,90 @@ const ReactSelect = ({
       {meta.touched && meta.error ? (
         <span className="custom-input-error">{meta.error.value}</span>
       ) : null}
-      <Formik>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Add a new Customer</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Customer does not exists. Please, add it!
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              name="name"
-              fullWidth
-              value={dialogValue.name}
-              onChange={(event) =>
-                setDialogValue({
-                  ...dialogValue,
-                  name: event.target.value,
-                })
-              }
-              label="name"
-              type="text"
-            />
-            <TextField
-              id="billingAddress"
-              label="Billing Address"
-              name="billingAddress"
-              value={billingAddress.address}
-              onChange={(event) =>
-                setBillingAddress({
-                  ...setBillingAddress,
-                  address: event.target.value,
-                })
-              }
-              fullWidth
-              margin="normal"
-              multiline
-              rows={4}
-              rowsMax={4}
-              variant="outlined"
-            />
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Add a new Customer</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Customer does not exists. Please, add it!
+          </DialogContentText>
+          <Formik
+            initialValues={initialCustomerValues}
+            //onSubmit={() => alert("submittsdfdsafadsf")}
+            onSubmit={(values, { resetForm }) => { handleNewCustomer(values.name,values.billingAddress)} }
+            validationSchema={validationSchemaCustomer}
+          >
+            {(props) => {
+              const {
+                values,
+                touched,
+                errors,
+                dirty,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                handleReset,
+              } = props;
+              return (
+                <Form onSubmit={handleSubmit}>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    name="name"
+                    fullWidth
+                    value={values.name}
+                    onChange={handleChange}
+                    //onChange={(event) =>
+                    //  setDialogValue({
+                    //    ...dialogValue,
+                    //    name: event.target.value,
+                    //  })
+                    //}
+                    label="name"
+                    type="text"
+                    helperText={errors.name && touched.name && errors.name}
+                    error={errors.name && touched.name}
+                  />
+                  <TextField
+                    id="billingAddress"
+                    label="Billing Address"
+                    name="billingAddress"
+                    value={values.address}
+                    onChange={handleChange}
+                    fullWidth
+                    //onBlur={handleBlur}
+                    //helperText={
+                    //  errors.description && touched.description && errors.description
+                    //}
+                    margin="normal"
+                    multiline
+                    rows={4}
+                    rowsMax={4}
+                    variant="outlined"
+                  />
+                  <Button
+                    type="submit"
+                    //onClick={() => handleNewCustomer()}
+                    color="primary"
+                  >
+                    Add
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                </Form>
+              );
+            }}
+            </Formik> 
           </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button
-              type="button"
-              onClick={() => handleNewCustomer()}
-              color="primary"
-            >
-              Add
-            </Button>
-          </DialogActions>
+          <DialogActions></DialogActions>
         </Dialog>
-      </Formik>
     </React.Fragment>
   );
 };
